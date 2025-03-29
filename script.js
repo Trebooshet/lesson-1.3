9200a297a9ba6c0122ab196d9079c22945e01af8
Sergei Goren <gorensergei@gmail.com>
2025-03-29 17:03:04 +0500

    wert

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 const checkboxReadMore = document.getElementById("checkbox_read_more")
 const labelReadMore = document.getElementById("label_read_more")
-const mainText = document.getElementById("main_text")
+const mainText = document.getElementById("main__text")
 
 labelReadMore.addEventListener("click", function(){
     labelReadMore.style.display = "none";
     mainText.style.height = "auto";
 })
 
+if (window.innerWidth >= 678) {
+    labelReadMore.style.display = "none";
+}
+else {
+    labelReadMore.style.display = "flex";
+}
 
+function resizeReadMore () {
+if (window.innerWidth >= 678) {
+    labelReadMore.style.display = "none";
+}
+else {
+    labelReadMore.style.display = "flex";
+}}
+
+window.addEventListener('resize', resizeReadMore)
