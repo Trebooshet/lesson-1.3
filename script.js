611d6316f38d301b8c2bf64e4f7f0c1b90a493d2
Sergei Goren <gorensergei@gmail.com>
2025-04-04 20:32:16 +0500

    w

--- script.js
+++ script.js
@@ -15,9 +15,10 @@ const headerMenuRight = document.querySelector(".header__menu-right")
 const burgerMenuIcons = document.querySelector(".burger-menu_icons")
 const header = document.querySelector("header")
 const main = document.querySelector("main")
+let isOpenAnyMenu = false;          /* Есть ли открытое меню */
 
 
-/* Изменение отображения, при изменении ширины экрана*/
+/* Изменение отображения кнопки "Читать далее", при изменении ширины экрана*/
  
 labelReadMore.addEventListener("click", function(){
     labelReadMore.style.display = "none";
@@ -43,87 +44,112 @@ else {
 
 window.addEventListener('resize', resizeReadMore)
 
-/* Burger menu */
 
- function resizeWindow () {
-    if (window.innerWidth >= 1440){
-        burgerMenu.style.display = "flex";
-        burgerMenu.style.height = "100vh";
-        burgerMenu.style.width = 320;
-        burgerCloseButton.style.display = "none";
-        document.querySelector("header").style.display = "none";
-        main.style.opacity = 1;
-        main.style.marginLeft = "320px";
-        main.style.width = "calc(100vw - 320px)";
-        resizeBox.appendChild(repairText);
-        resizeBox.appendChild(repairLogo);
-        resizeBox.appendChild(checkStatusText);
-        resizeBox.appendChild(checkStatusLogo);
-        burgerMenuIcons.appendChild(callLogo);
-        burgerMenuIcons.appendChild(chatLogo);
-        burgerMenuIcons.appendChild(profileLogo);
-    }
-    else {
-        burgerCloseButton.style.display = "flex";
-        burgerMenu.style.display = "none";
-        header.style.display = "flex";
-        header.style.opacity = 1;
-        header.style.marginLeft = 0;
-        header.style.width = "100vw";
-        main.style.opacity = 1;
-        main.style.marginLeft = 0;
-        main.style.width = "100vw";
-        headerMenuRight.appendChild(repairText);
-        headerMenuRight.appendChild(repairLogo);
-        headerMenuRight.appendChild(checkStatusText);
-        headerMenuRight.appendChild(checkStatusLogo);
-        headerMenuRight.appendChild(callLogo);
-        headerMenuRight.appendChild(chatLogo);
-        headerMenuRight.appendChild(profileLogo)
-    }
+/* Адаптация контента в зависимости от ширины экрана */
+
+ function resizeWindow () {       
+    if (!isOpenAnyMenu) {               /* Нет открытых меню*/
+
+        if (window.innerWidth >= 1440){
+            burgerMenu.style.display = "flex";
+            burgerMenu.style.height = "100vh";
+            burgerMenu.style.width = "320px";
+            burgerCloseButton.style.display = "none";
+            document.querySelector("header").style.display = "none";
+            main.style.opacity = 1;
+            main.style.marginLeft = "320px";
+            main.style.width = "calc(100vw - 320px)";
+            resizeBox.appendChild(repairText);
+            resizeBox.appendChild(repairLogo);
+            resizeBox.appendChild(checkStatusText);
+            resizeBox.appendChild(checkStatusLogo);
+            burgerMenuIcons.appendChild(callLogo);
+            burgerMenuIcons.appendChild(chatLogo);
+            burgerMenuIcons.appendChild(profileLogo);
+        }
+        else {
+            burgerCloseButton.style.display = "flex";
+            burgerMenu.style.display = "none";
+            header.style.display = "flex";
+            header.style.opacity = 1;
+            header.style.marginLeft = 0;
+            header.style.width = "100vw";
+            main.style.opacity = 1;
+            main.style.marginLeft = 0;
+            main.style.width = "100%";
+            headerMenuRight.appendChild(repairText);
+            headerMenuRight.appendChild(repairLogo);
+            headerMenuRight.appendChild(checkStatusText);
+            headerMenuRight.appendChild(checkStatusLogo);
+            headerMenuRight.appendChild(callLogo);
+            headerMenuRight.appendChild(chatLogo);
+            headerMenuRight.appendChild(profileLogo)
+        }
+    }   
+    else if (isOpenAnyMenu && window.innerWidth >= 1440) { /* Есть открытое меню и ширина > 1440 */
+            closeBurgerMenu();
+            burgerMenu.style.boxShadow = "0 0 0";
+            burgerMenu.style.display = "flex";
+            burgerMenu.style.height = "100vh";
+            burgerMenu.style.width = "320px";
+            burgerCloseButton.style.display = "none";
+            document.querySelector("header").style.display = "none";
+            main.style.opacity = 1;
+            main.style.marginLeft = "320px";
+            main.style.width = "calc(100vw - 320px)";
+            resizeBox.appendChild(repairText);
+            resizeBox.appendChild(repairLogo);
+            resizeBox.appendChild(checkStatusText);
+            resizeBox.appendChild(checkStatusLogo);
+            burgerMenuIcons.appendChild(callLogo);
+            burgerMenuIcons.appendChild(chatLogo);
+            burgerMenuIcons.appendChild(profileLogo);
+    }    
 }
 
-window.addEventListener("resize", resizeWindow)
+window.addEventListener("resize", resizeWindow) /* Адаптация контента при загрузке страницы */
+window.addEventListener("load", resizeWindow)   /* Адаптация контента при изменении ширины страницы */
 
-window.addEventListener("load", resizeWindow)
+/* Burger menu */
 
+burgerButton.addEventListener("click", openBurgerMenu); /* Открыть BurgerMenu кнопкой BurgerButton*/
 
-burgerButton.addEventListener("click",() => {
-    
+function openBurgerMenu() {             /* Функция открыть BurgerMenu*/
+    burgerMenu.style.display = "flex";
+    burgerMenuIcons.appendChild(callLogo);
+    burgerMenuIcons.appendChild(chatLogo);
+    burgerMenuIcons.appendChild(profileLogo);
+    isOpenAnyMenu = true; 
+
     if (window.innerWidth >= 510) {
-        burgerMenu.style.display = "flex";
-        burgerMenuIcons.appendChild(callLogo);
-        burgerMenuIcons.appendChild(chatLogo);
-        burgerMenuIcons.appendChild(profileLogo);
         header.style.opacity = 0.05;
         main.style.opacity = 0.05;
         burgerMenu.style.height = "100vh";
         burgerMenu.style.boxShadow = "1px 0 40px rgb(172, 170, 200)";
-        return
     }
     else if (window.innerWidth >= 320) {
-        burgerMenu.style.display = "flex";
         burgerMenu.style.width = "100%";
-        burgerMenuIcons.appendChild(callLogo);
-        burgerMenuIcons.appendChild(chatLogo);
-        burgerMenuIcons.appendChild(profileLogo);
-        return
     }
     else {
-        burgerMenu.style.display = "flex";
-        burgerMenu.style.width = "320";
-        burgerMenuIcons.appendChild(callLogo);
-        burgerMenuIcons.appendChild(chatLogo);
-        burgerMenuIcons.appendChild(profileLogo);
-        return
+        burgerMenu.style.width = "320px";
+    }
+}
+
+burgerCloseButton.addEventListener("click", closeBurgerMenu); /* Закрыть BurgerMenu кнопкой*/
+
+document.addEventListener("click", (event) => {  /* Закрыть BurgerMenu кликом по пустой области*/
+    if (!burgerMenu.contains(event.target) && !burgerButton.contains(event.target)) {
+        closeBurgerMenu()
     }
 })
 
-burgerCloseButton.addEventListener("click",() => {
+function closeBurgerMenu() {    /* Функция закрыть BurgerMenu */
     burgerMenu.style.display = "none";
     headerMenuRight.appendChild(callLogo);
     headerMenuRight.appendChild(chatLogo);
     headerMenuRight.appendChild(profileLogo);
     header.style.opacity = 1;
     main.style.opacity = 1;
-})
+    isOpenAnyMenu = false; 
+    
+}
