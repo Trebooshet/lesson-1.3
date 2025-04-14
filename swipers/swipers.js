55c4b05b5fdc9e36e85fa6dfb56f736a9804035d
Sergei Goren <gorensergei@gmail.com>
2025-04-14 21:44:27 +0500

    w

--- swipers/swipers.js
+++ swipers/swipers.js
@@ -27,3 +27,50 @@ new Swiper('.brend-swiper', {
     slidesPerView: 2,
     spaceBetween: 24,
   });
+
+const brendGrid = document.querySelector(".brend-grid");
+const showAllBrendsButton = document.querySelector("#show-all-brends_button");
+const brendsDoubleArrow = document.querySelector("#brends_double-arrow");
+const showAllBrendsButtonText = document.querySelector(".show-all-title");
+let isAllBrendsShown = false;
+const technicGrid = document.querySelector(".technic-grid");
+const showAllTechnicsButton = document.querySelector("#show-all-technics_button");
+const showAllTechnicsButtonText = document.querySelector(".show-all-title");
+const technicsDoubleArrow = document.querySelector("#technics_double-arrow");
+let isAllTechnicsShown = false;
+
+function showAllBrends() {
+    brendGrid.style.height = "auto";
+    showAllBrendsButtonText.innerHTML = "Скрыть";
+    brendsDoubleArrow.style.transform = "scaleY(-1)";
+    isAllBrendsShown = true;
+}
+
+function hideAllBrends() {
+    brendGrid.style.height = "168px";
+    showAllBrendsButtonText.innerHTML = "Показать все";
+    brendsDoubleArrow.style.transform = "scaleY(1)"
+    isAllBrendsShown = false;
+}
+
+showAllBrendsButton.addEventListener("click", () => {
+    !isAllBrendsShown ? showAllBrends() : hideAllBrends();
+})
+
+function showAllTechnics() {
+    technicGrid.style.height = "auto";
+    showAllTechnicsButtonText.innerHTML = "Скрыть";
+    technicsDoubleArrow.style.transform = "scaleY(-1)";
+    isAllTechnicsShown = true;
+}
+
+function hideAllTechnics() {
+    technicGrid.style.height = "168px";
+    showAllTechnicsButtonText.innerHTML = "Показать все";
+    technicsDoubleArrow.style.transform = "scaleY(1)"
+    isAllTechnicsShown = false;
+}
+
+showAllTechnicsButton.addEventListener("click", () => {
+    !isAllTechnicsShown ? showAllTechnics() : hideAllTechnics();
+})
